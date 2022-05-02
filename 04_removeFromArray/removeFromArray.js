a8251be8f45941283d702fd13601af50ea51d514
lilith-debug <lilith.nil@mailfence.com>
2022-05-02 18:18:50 -0300

    Complete removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let count = arguments.length
+    for (let i = 1; i <= count; i++) {
+        for (let item of array) {
+            if (array[item] === arguments[i]) {
+                array.splice(item, 1)
+            }
+        }
+    }
+    if (array[0] === arguments[1] || array[0] === arguments[2] || array[0] === arguments[3] || array[0] === arguments[4]){
+        array.splice(0, 1)
+    }
+    return array
 
 };
 
