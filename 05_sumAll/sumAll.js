a74899345898a5b4ae4ae69a06cca170e51de3c6
lilith-debug <lilith.nil@mailfence.com>
2022-05-03 17:15:18 -0300

    Complete sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (int1 < 0 || int2 < 0 || typeof int1 !== "number" || typeof int2 !== "number") {
+        return "ERROR"
+    }
+    else {
+        if (int1 < int2) {
+            let sum = int1;
+            for (let i = int1+1; i <= int2; i++){
+                sum += i;
+            }
+            return sum
+        }
+        else if (int1 > int2) {
+            let sum = int2;
+            for (let i = int2+1; i <= int1; i++){
+                sum += i;
+            }
+            return sum
+        }
+    }
 };
 
 // Do not edit below this line
