3d12f08d87cb62b33111720ac67635a0dbd14f72
lilith-debug <lilith.nil@mailfence.com>
2022-05-09 18:27:55 -0300

    Complete palindromes function

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase()
+    const array = Array.from(str);
+    const letters = Array.from("abcdefghijklmnopqrstuvwxyz");
+    const filtered = array.filter(letter => letters.includes(letter));
+    const reversed = [];
+    for (let i = filtered.length - 1; i >= 0; i--) {
+        reversed.push(filtered[i]);
+    }
+    return filtered.join('') === reversed.join('')
 };
 
 // Do not edit below this line
