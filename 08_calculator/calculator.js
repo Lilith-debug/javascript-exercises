ed58ec29a8177a38713f3b5c374fca3f6719f554
lilith-debug <lilith.nil@mailfence.com>
2022-05-09 17:18:16 -0300

    Complete calculator functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const result = array.reduce((total, number) => total += number, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const result = array.reduce((total, number) => total *= number, array[0]) / array[0];
+  return result;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) {
+    return 1;
+  } else {
+    let total = number;
+    for (let i = number - 1; i > 0; i--){
+      total *= i;
+    }
+    return total;
+  }
 };
 
 // Do not edit below this line
