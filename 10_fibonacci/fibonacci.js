1f851a81c5e3c1dba397bd10ba7fba6715ef309d
lilith-debug <lilith.nil@mailfence.com>
2022-05-09 18:53:34 -0300

    Complete fibonacci function

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(sequence) {
+    Number(sequence);
+    let first = 0;
+    let second = 1;
+    if (sequence > 0) {
+        for (let i = 1; i <= sequence; i++) {
+            if (i % 2 != 0) {
+                second += first;
+            } else if (i % 2 == 0) {
+                first += second;
+            }
+        }
+        if (sequence % 2 != 0) {
+            return second;
+        } else if (sequence % 2 == 0) {
+            return first;
+        }
+    } else {
+        return "OOPS"
+    }
 };
 
 // Do not edit below this line
