2e3bc97c22023b6ec770990c2fe1d9d621b4020f
lilith-debug <lilith.nil@mailfence.com>
2022-05-10 17:17:52 -0300

    Complete findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    for (let person of people) {
+        if (!('yearOfDeath' in person)) {
+            person.yearOfDeath = currentYear;
+        }
+    };
+    people.sort((last, next) => {
+    const lastPerson = last.yearOfDeath - last.yearOfBirth;
+    const nextPerson = next.yearOfDeath - next.yearOfBirth;
+    return lastPerson > nextPerson ? -1 : 1;
+    });
+    return people[0];
 };
 
 // Do not edit below this line
